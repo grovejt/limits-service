1c24ac7e82098b23905aea350b412db6bd93fc7f
grovejt <grovejt@gmail.com>
2018-09-21 15:36:45 -0500

    Added in Spring Cloud Sleuth for distributed log tracing

--- src/main/java/com/grove/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/grove/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,6 +3,9 @@ package com.grove.microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,14 @@ public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+	
+	/**
+	 * Enable distributed tracing with Spring Cloud Sleuth.
+	 * @return
+	 */
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
