681ae80507c718fd14989888594e4b13d63edeba
grovejt <grovejt@gmail.com>
2018-09-23 14:54:19 -0500

    added Zipkin integration

--- src/main/java/com/grove/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/grove/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,7 @@
 package com.grove.microservices.limitsservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,15 @@ import com.grove.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		
+		logger.info("retrieveLimitsFromConfigurations called");
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
